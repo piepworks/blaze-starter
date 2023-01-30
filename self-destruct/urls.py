279625de641cbd6c912b516d9a2b73ccf917d453
Trey Piepmeier <trey@treypiepmeier.com>
2023-01-30 11:46:27 -0500

    Add placeholder homepage.
    So the first thing you see isn't an error page.

--- self-destruct/urls.py
+++ self-destruct/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    # TODO: Remove this template and add your own.
+    path(
+        "", TemplateView.as_view(template_name="placeholder.html"), name="placeholder"
+    ),
     # TODO: Change the `admin/` path to something custom.
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
