29e14cb0198602ccba5483f6ee53e12522963257
Trey Piepmeier <trey@treypiepmeier.com>
2023-05-09 12:01:43 -0400

    Spruce up and fix the setup script.
    - Fix verb tenses
    - `set -e` was making the JS steps break for some reason.
    - `pre-commit install` was failing running before `git init`.
      No idea why it's doing that all of a sudden. I thought it had worked.

--- dev/setup.sh
+++ dev/setup.sh
@@ -3,9 +3,6 @@
 # This is to initially set up a project. You probably don't need to
 # ever run it again.
 
-# Allow exiting the script with ctrl+c any time.
-set -e
-
 export GUM_SPIN_SPINNER='line'
 export GUM_SPIN_SHOW_OUTPUT=true
 
@@ -16,6 +13,7 @@ format_python_friendly () {
 }
 
 require () {
+  # Allow exiting the loop with ctrl+c.
   set -e
   while true; do
     input=$(eval "$@")
@@ -65,17 +63,16 @@ fi
 # Setup Python stuff
 gum style --border normal --margin "1" --padding "0 2" --border-foreground 212 \
   "Installing $(gum style --foreground 212 'Python') goodies…"
-gum spin --title "Set up Python virtual environment" -- python3 -m venv --prompt . .venv
-gum spin --title "Warm up virtual environment" -- .venv/bin/pip install -U pip setuptools wheel
+gum spin --title "Setting up Python virtual environment" -- python3 -m venv --prompt . .venv
+gum spin --title "Warming up virtual environment" -- .venv/bin/pip install -U pip setuptools wheel
 source .venv/bin/activate
-gum spin --title "Warm up virtual environment" -- python -m pip install pip-tools Django
-gum spin --title "Install Django Starter" -- django-admin startproject \
+gum spin --title "Warming up virtual environment" -- python -m pip install pip-tools Django
+gum spin --title "Installing Django Starter" -- django-admin startproject \
   --extension=ini,py,toml,yaml,yml \
   --template=https://github.com/piepworks/django-starter/archive/main.zip \
   $PROJECT_NAME .
-gum spin --title "Install Python dependencies" -- pip-compile --resolver=backtracking requirements/requirements.in
-gum spin --title "Install Python dependencies" -- python -m pip install -r requirements/requirements.txt
-gum spin --title "Install Python dependencies" -- pre-commit install
+gum spin --title "Installing Python dependencies" -- pip-compile --resolver=backtracking requirements/requirements.in
+gum spin --title "Installing Python dependencies" -- python -m pip install -r requirements/requirements.txt
 
 # Setup .env file
 echo "SECRET_KEY=$(eval ./dev/generate-django-key)" >> .env
@@ -87,21 +84,22 @@ echo "DATABASE_URL=sqlite:///db.sqlite3" >> .env
 # Warm up the database and static files
 gum style --border normal --margin "1" --padding "0 2" --border-foreground 212 \
   "Warming up $(gum style --foreground 212 'database') and $(gum style --foreground 212 'static files')…"
-gum spin --title "Collect static files" -- python manage.py collectstatic
-gum spin --title "Database warmup" -- python manage.py makemigrations
-gum spin --title "Database warmup" -- python manage.py migrate
-gum spin --title "Create initial superuser account" -- python manage.py createsuperuser \
+gum spin --title "Collecting static files" -- python manage.py collectstatic
+gum spin --title "Warming up the database" -- python manage.py makemigrations
+gum spin --title "Warming up the database" -- python manage.py migrate
+gum spin --title "Creating initial superuser account" -- python manage.py createsuperuser \
   --noinput --username=$USERNAME --email=$EMAIL
 
 # Setup JS stuff
 gum style --border normal --margin "1" --padding "0 2" --border-foreground 212 \
   "Installing $(gum style --foreground 212 'JavaScript') goodies…"
 source $HOME/.nvm/nvm.sh
-gum spin --title "Make sure we’re using the right version of Node" -- nvm install
-gum spin --title "Install JavaScript dependencies" -- npm install
+gum spin --title "Making sure we’re using the right version of Node" -- nvm install
+gum spin --title "Installing JavaScript dependencies" -- npm install
 
 # Start a new Git project
 git init --initial-branch=main&&git add .&&git commit -m "New project from Piepwork's Django Starter."
+gum spin --title "Installing Git pre-commit hooks" -- pre-commit install
 
 # Explain next steps
 gum format -- \
