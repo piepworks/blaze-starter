3381d50739ebe0b5d65fdd436fdc9d9a898ecdf4
Trey Piepmeier <trey@treypiepmeier.com>
2023-05-20 18:29:43 -0400

    Favicon goodies!
    https://adamj.eu/tech/2022/01/18/how-to-add-a-favicon-to-your-django-site/

--- config/tests/test_views.py
+++ config/tests/test_views.py
@@ -1,5 +1,8 @@
 # import pytest
+from http import HTTPStatus
 
 
-def test_a_view():
-    pass
+def test_favicon(client):
+    response = client.get("/favicon.ico")
+    assert response.status_code == HTTPStatus.OK
+    assert response["Cache-Control"] == "max-age=86400, immutable, public"
