70a9cf742e7d491e22d495bbd091f169ff07afb1
Trey Piepmeier <trey@treypiepmeier.com>
2023-06-02 10:34:26 -0400

    Add ESLint and a placeholder JS file.

---
+++ templates/js/placeholder.js
@@ -0,0 +1,7 @@
+// JS files with Django template variables are cool! But be careful.
+
+// {% if user.is_authenticated %}
+console.log('You are logged in');
+// {% else %}
+console.log('You are not logged in');
+// {% endif %}
