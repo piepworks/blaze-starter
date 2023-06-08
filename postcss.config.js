471daa10745d994464db2dc2a9753f524748a136
Trey Piepmeier <trey@treypiepmeier.com>
2023-06-08 12:09:15 -0400

    Update footer, change CSS nesting to follow spec.
    https://chriscoyier.net/2023/03/02/nesting-in-postcss-which-way-to-go/

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-nested': {},
+    'postcss-nesting': {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
