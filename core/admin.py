c3fceee894c492fc911d7bc12a10e7839d230fbb
Trey Piepmeier <trey@treypiepmeier.com>
2023-06-09 09:24:46 -0400

    Remove Group from admin.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
 from .models import User
 
 
@@ -37,3 +38,6 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
+
+
+admin.site.unregister(Group)
