0a487816ced627de266066591b93a9a977012b02
Trey Piepmeier <trey@treypiepmeier.com>
2023-06-16 09:45:17 -0400

    Add test coverage for models.py
    It's nice that this article was featured in the same issue of Django
    News that featured our first Blaze.horse ad!
    
    https://www.revsys.com/tidbits/pytest-fixtures-are-magic/
    
    via https://django-news.com/issues/184?#start

--- config/tests/test_models.py
+++ config/tests/test_models.py
@@ -1,5 +1,45 @@
-# import pytest
+import pytest
 
 
-def test_a_model():
-    pass
+@pytest.fixture
+def create_user(django_user_model):
+    def inner_function(email, password):
+        return django_user_model.objects.create_user(email, password)
+
+    return inner_function
+
+
+@pytest.fixture
+def create_superuser(django_user_model):
+    def inner_function(email, password, **extra_fields):
+        return django_user_model.objects.create_superuser(
+            email, password, **extra_fields
+        )
+
+    return inner_function
+
+
+def test_user_without_email(create_user):
+    with pytest.raises(ValueError):
+        create_user("", "")
+
+
+@pytest.mark.django_db
+def test_user_creation(create_user):
+    user = create_user("user@example.com", "")
+    assert str(user) == "user@example.com"
+
+
+def test_superuser_creation(create_superuser):
+    superuser = create_superuser("superuser@example.com", "")
+    assert str(superuser) == "superuser@example.com"
+
+
+def test_superuser_not_staff(create_superuser):
+    with pytest.raises(ValueError):
+        create_superuser("superuser@example.com", "", is_staff=False)
+
+
+def test_superuser_not_superuser(create_superuser):
+    with pytest.raises(ValueError):
+        create_superuser("superuser@example.com", "", is_superuser=False)
