c42cdff4c8ee861e499206b9c1c7bfb4f9793511
Trey Piepmeier <trey@treypiepmeier.com>
2023-06-16 17:30:45 -0400

    Fix placeholder homepage and test for it.

--- config/tests/test_views.py
+++ config/tests/test_views.py
@@ -6,3 +6,13 @@ def test_favicon(client):
     response = client.get("/favicon.ico")
     assert response.status_code == HTTPStatus.OK
     assert response["Cache-Control"] == "max-age=86400, immutable, public"
+
+
+def test_homepage(client, settings):
+    settings.STORAGES = {
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"
+        }
+    }
+    response = client.get("/")
+    assert response.status_code == HTTPStatus.OK
