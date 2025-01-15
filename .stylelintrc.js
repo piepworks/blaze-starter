565a28517576b64483cbdb4ba3a9e35b714602e7
Trey Piepmeier <trey@treypiepmeier.com>
2025-01-15 09:30:07 -0500

    Update stylelint stuff.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,11 @@ module.exports = {
   rules: {
     'declaration-empty-line-before': 'never',
     'no-descending-specificity': null,
+    'media-feature-range-notation': null,
+    'comment-empty-line-before': null,
+    'selector-id-pattern': '^[a-z][a-z0-9]*([_|-][a-z0-9]+)*$', // Snake, kebab cases
+    'value-keyword-case': null,
+    'at-rule-descriptor-no-unknown': null,
   },
   ignoreFiles: ['static_src/css/vendor/**'],
 };
